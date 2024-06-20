d33332bf483d4103abe4b84ce0981ae3b3b25aa1
Uloma Okenyi <cyndibilo@gmail.com>
2024-06-20 15:02:20 +0100

    Added Domain Project and Entities

---
+++ Greggs.Product.Domain/Entities/NutritionalInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greggs.Product.Domain.Entities
+{
+    public class NutritionalInformation
+    {
+        public int NutritionID { get; set; }
+        public int ProductID { get; set; }
+        public int NutrientID { get; set; }
+        public decimal Measurement { get; set; }
+        public decimal NutritionalValue { get; set; }
+
+        public Product Product { get; set; }
+        public Nutrient Nutrient { get; set; }
+    }
+}
---
+++ Greggs.Product.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greggs.Product.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; } 
+        public string ProductName { get; set; }
+        public decimal PriceInPounds { get; set; }
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public Category Category { get; set; }
+
+        public List<NutritionalInformation> NutritionalInformations { get; set; }
+
+    }
+}
